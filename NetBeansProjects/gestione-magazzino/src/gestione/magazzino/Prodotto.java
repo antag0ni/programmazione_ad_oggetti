eb363286c7a50b05e9ffbdf44918ed35c54ff246
Antagoni <gionatan.conti04@gmail.com>
2026-09-16 10:49:43 +0200

    first lesson

---
+++ NetBeansProjects/gestione-magazzino/src/gestione/magazzino/Prodotto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestione.magazzino;
+
+/**
+ *
+ * @author antagoni
+ */
+public class Prodotto {
+    int codice; // variabili di istanza, non serve inizializzarle
+    String descrizione;
+    String dataDiProduzione;
+    float costo;
+}
