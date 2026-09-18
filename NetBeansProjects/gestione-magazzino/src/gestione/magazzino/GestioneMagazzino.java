9a2d8640624535d7389bb434ae7b60fcf09834c5
Antagoni <gionatan.conti04@gmail.com>
2026-09-18 16:39:26 +0200

    18-09-2026

--- NetBeansProjects/gestione-magazzino/src/gestione/magazzino/GestioneMagazzino.java
+++ NetBeansProjects/gestione-magazzino/src/gestione/magazzino/GestioneMagazzino.java
@@ -16,13 +16,39 @@ public class GestioneMagazzino {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+        
         // Le variabili devono essere sempre inizializzate
         Prodotto p;
         p = new Prodotto(); // p contiene il riferimento all'area di memoria che contiene l'oggetto 
-        System.out.println(p.codice);
-        p.codice = 1;
-        System.out.println(p.codice);
-        System.out.println(p.dataDiProduzione);
+        
+        Prodotto p1 = new Prodotto();
+        
+        // NON ACCESSIBILI SE PRIVATI
+        //System.out.println(p.codice);
+        //p.codice = 1;
+        //System.out.println(p.codice);
+        //System.out.println(p.dataDiProduzione);
+        
+        //System.out.println(p.getCodice());
+        p.setCodice(10);
+        p.setDataDiProduzione("2015-11-12");
+        p.setDescrizione("Tavolo");
+        p.setCosto(12.50F); //va specificato il tipo altrimenti il default è double
+        
+        p.stampaInfo();
+        
+        p1.stampaInfo();
+        
+        Prodotto p2 = new Prodotto(25, "2025-12-20", "Penna", 6.0F);
+        
+        p2.stampaInfo();
+        
+        Prodotto p3;
+        p3 = p2.clona();
+        
+        p3.setCosto(1.5F);
+        
+        p3.stampaInfo();
     }
 }
--- NetBeansProjects/gestione-magazzino/src/gestione/magazzino/Prodotto.java
+++ NetBeansProjects/gestione-magazzino/src/gestione/magazzino/Prodotto.java
@@ -10,8 +10,77 @@ package gestione.magazzino;
  * @author antagoni
  */
 public class Prodotto {
-    int codice; // variabili di istanza, non serve inizializzarle
-    String descrizione;
-    String dataDiProduzione;
-    float costo;
+    
+    // Spazio attributi
+    
+    private int codice; // variabili di istanza, non serve inizializzarle
+    private String descrizione;
+    private String dataDiProduzione;
+    private float costo;
+    
+    // Spazio metodi
+    
+    //COSTRUTTORE
+    
+    public Prodotto() {
+        this.codice = -1;
+        this.dataDiProduzione = "0000-00-00";
+        this.descrizione = "Non disponibile";
+        this.costo = 0.0F;
+    }
+    
+    public Prodotto(int codice, String dataDiProduzione, String descrizione, float costo) {
+        this.codice = codice;
+        this.dataDiProduzione = dataDiProduzione;
+        this.descrizione = descrizione;
+        this.costo = costo;
+    }
+    
+    public int getCodice() {
+        return codice;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public String getDataDiProduzione() {
+        return dataDiProduzione;
+    }
+    public float getCosto() {
+        return costo;
+    }
+    
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public void setDataDiProduzione(String dataDiProduzione) {
+        this.dataDiProduzione = dataDiProduzione;
+    }
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+    
+    public Prodotto clona() {
+        return new Prodotto(this.codice, this.dataDiProduzione, this.descrizione, this.costo);
+    }
+    
+    public void stampaInfo() {
+        
+        // String descrizione = "Ciao!"; <-- variabile locale
+        
+        System.out.println("*** Prodotto ***");
+        System.out.print("Codice: ");
+        System.out.println(codice);
+        
+        System.out.print("Descrizione: ");
+        System.out.println(descrizione);
+        
+        System.out.print("Costo: ");
+        System.out.println(costo);
+        
+        System.out.print("Data di produzione: ");
+        System.out.println(dataDiProduzione);
+    }   
 }
